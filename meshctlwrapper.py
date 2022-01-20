b54a979b116c983ebf1a2ab0fdc7e9b6bce8139f
deez <deez@nutes.com>
2022-01-20 21:10:44 +0000

    Finished discover nodes and started implementing provisioning

--- meshctlwrapper.py
+++ meshctlwrapper.py
@@ -33,15 +33,14 @@ def connectNetwork():
 def getAllNodes():
     msg = ""
     with open("/home/pi/.config/meshctl/prov_db.json", 'r') as file:
+        msg = ""
         data = json.load(file)
-        
         for i, p in enumerate(data['nodes']):
-            msg += ('node: ' + str(i))
             for e in p['configuration']['elements']:
                 if "models" in e:
-                    msg += e
-            msg += "----------------------------------------------------------\n"
-    return msg
+                    msg += str(e) + "\n"
+                    # msg += e['unicastAddress'] + "\n"
+        return msg
 
 def toggleLight(target, onoff):
     global process
@@ -95,19 +94,41 @@ def getResponse(p):
         except:
             break
 
-def startDiscoverUnprovisioned(seconds):
+def startDiscoverUnprovisioned():
     global process 
-    #Start discovering with time specified
-    process.stdin.write(("discover-unprovisioned on " + seconds).encode())
+    process.stdin.write("back\n".encode())
     time.sleep(0.5)
+    process.stdin.write("discover-unprovisioned on\n".encode())
+    print("Discovering... Please wait 10 seconds.")
+    time.sleep(10)
+    UUIDs = []
     while 1:
         try:
             response = process.stdout.readline().decode('utf8')
             if ('Device UUID: ' in response):
-                aresponse = process.stdout.readline().decode('utf8')
-                return aresponse
+                index = response.find("Device UUID: ")
+                UUIDs.append(response[index+13:])
+                return UUIDs[0]
         except:
             print('No unprovioned devices found.')
             break
 
+def provisionNode(UUID):
+    global process 
+    process.stdin.write("back\n".encode())
+    time.sleep(0.5)
+    process.stdin.write(("provision " + str(UUID) + "\n").encode())
+    print("Provisioning device... Please wait 10 seconds.")
+    time.sleep(10)
+    while 1:
+        try:
+            response = process.stdout.readline().decode('utf8')
+            if ('Agent String: Push' in response):
+                index = response.find("Push")
+                print("Push button " + response[index+5:index+6] + "x times to provision.")
+                return 1
+        except:
+            print('Provisioning failed')
+            break
+    
 init()
